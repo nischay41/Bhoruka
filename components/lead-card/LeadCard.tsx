57a756ad68bab87e00cdadb3f16ec8f04f55b7a7
ketanjain7981 <ketan.jain@think41.com>
2025-06-20 16:27:56 +0530

    chore: update demo phone number in lead card component

--- components/lead-card/LeadCard.tsx
+++ components/lead-card/LeadCard.tsx
@@ -239,7 +239,7 @@ const LeadCard: React.FC<LeadCardProps> = ({ lead }) => {
         },
         body: JSON.stringify({
           name: lead.name,
-          phone: '+919672618163',
+          phone: '+919682194502',
           usecase_id: 'c4908121-d6ec-4fac-bf71-2769da7ed90d',
           email: 'ketan.demo@example.com',
         }),
